bb8c127e39a46aaaf03a4ad986cbd2fd40fa97b7
hqabelardosalazar <salazar@hqrentalsoftware.com>
2026-04-12 12:52:30 -0400

    fix: remove HeroSection MotionReveal

--- src/components/HomeSections/HeroSection.tsx
+++ src/components/HomeSections/HeroSection.tsx
@@ -9,7 +9,6 @@ import {
   Text,
 } from "@/components/ui/ui-wrapper";
 import { FileText } from "lucide-react";
-import { MotionReveal } from "../ui/MotionReveal"; // Importando tu componente
 import { downloadCV } from "@/utils/download-cv";
 import { ScrollIndicator } from "../navigation/ScrollIndicator";
 
@@ -24,37 +23,35 @@ export const HeroSection = () => {
       <div className="absolute inset-0 -z-10 bg-[radial-gradient(circle_at_50%_50%,rgba(var(--primary-rgb),0.04),transparent_70%)]" />
 
       <Container size="lg">
-        <MotionReveal delay={0.2}>
-          <div className="text-center space-y-8">
-            <div className="flex justify-center">
-              <Badge variant="secondary" className="px-4 py-1.5 gap-2 ">
-                {t("available")}
-              </Badge>
-            </div>
-
-            <Heading level="h1" className="mx-auto">
-              {t("title")}
-            </Heading>
-
-            <Container size="sm" className="px-0">
-              <Text size="lead" className="text-base-content leading-relaxed">
-                {t("description")}
-              </Text>
-            </Container>
-
-            <div className="flex flex-col sm:flex-row  justify-center items-center pt-6">
-              <Button
-                variant="outline"
-                size="lg"
-                className="px-10 rounded-full gap-2"
-                onClick={handleCVDownlodad}
-              >
-                <FileText className="w-4 h-4" />
-                {t("cv")}
-              </Button>
-            </div>
+        <div className="text-center space-y-8">
+          <div className="flex justify-center">
+            <Badge variant="secondary" className="px-4 py-1.5 gap-2 ">
+              {t("available")}
+            </Badge>
           </div>
-        </MotionReveal>
+
+          <Heading level="h1" className="mx-auto">
+            {t("title")}
+          </Heading>
+
+          <Container size="sm" className="px-0">
+            <Text size="lead" className="text-base-content leading-relaxed">
+              {t("description")}
+            </Text>
+          </Container>
+
+          <div className="flex flex-col sm:flex-row  justify-center items-center pt-6">
+            <Button
+              variant="outline"
+              size="lg"
+              className="px-10 rounded-full gap-2"
+              onClick={handleCVDownlodad}
+            >
+              <FileText className="w-4 h-4" />
+              {t("cv")}
+            </Button>
+          </div>
+        </div>
       </Container>
       <ScrollIndicator targetId="experience" />
     </section>
